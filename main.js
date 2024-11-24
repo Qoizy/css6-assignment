c202c555a9f3a2b9a90279cc46950a161bb0105e
Oladimeji Quyum <oladimejiquyum30@gmail.com>
2024-11-24 19:22:17 +0100

    Add CSS6 assignment files

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+    document.querySelector(".nav-actions").classList.toggle("active2");
+  }
+
+  
+
+
+
+
+
